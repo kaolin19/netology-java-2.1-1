05e53d8b19bcdb92fc40b42a8fd2c665b06087d2
Boris Jarczuk <yarchuk.b@gmail.com>
2020-12-14 19:12:13 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPriceKopecks = 715900;
+        int divisorValueKopecks = 2000;
+        int bonusMiles = ticketPriceKopecks / divisorValueKopecks;
+        System.out.println(bonusMiles);
+    }
+}
